1c0dabaa61af387b57016b655e3fdce4f7478ab8
Jam Risser <jam@jamrizzi.com>
2018-01-04 01:38:18 -0600

    Update dsrp.sh

--- dsrp.sh
+++ dsrp.sh
@@ -2,13 +2,13 @@
 
 docker run -d --name nginx --restart=always -p 80:80 -p 443:443 \
        --name nginx-proxy \
-       -v /volumes/certs:/etc/nginx/certs:ro \
+       -v /volumes/certs:/etc/nginx/certs \
        -v /etc/nginx/vhost.d \
        -v /usr/share/nginx/html \
-       -v /var/run/docker.sock:/tmp/docker.sock:ro \
+       -v /var/run/docker.sock:/tmp/docker.sock \
        jwilder/nginx-proxy
 docker run -d --name letsencrypt --restart=unless-stopped \
-       -v /volumes/certs:/etc/nginx/certs:rw \
+       -v /volumes/certs:/etc/nginx/certs \
        --volumes-from nginx-proxy \
-       -v /var/run/docker.sock:/var/run/docker.sock:ro \
+       -v /var/run/docker.sock:/var/run/docker.sock \
        jrcs/letsencrypt-nginx-proxy-companion:latest
